c4198c9df363b024cf0840be65b28ff4edcbcf05
Tim 'mithro' Ansell <me@mith.ro>
2018-11-26 17:22:41 -0800

    Small changes.

--- hw/bsp/valentyusb/board.mk
+++ hw/bsp/valentyusb/board.mk
@@ -4,15 +4,15 @@ CFLAGS = \
 	-DCFG_TUSB_MCU=OPT_MCU_LM32 \
 
 # All source paths should be relative to the top level.
-LD_FILE = hw/bsp/metro_m0_express/samd21g18a_flash.ld
+LD_FILE = hw/bsp/valentyusb/linker.ld
 
 LD_FLAGS +=
 
-SRC_C += \
-	-I$(TOP)/hw/mcu/valentyusb/main.c
+#SRC_C += \
+#	-I$(TOP)/hw/mcu/valentyusb/main.c
 
 INC += \
 	-I$(TOP)/hw/mcu/valentyusb/
 
-VENDOR = valentyusb
+VENDOR = #valentyusb
 CHIP_FAMILY = valentyusb
